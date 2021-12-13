dae9a9661b8e151a36f643e087099ae953335b28
Alejandro <alejandro.areco@istea.com.ar>
2021-12-13 07:46:39 -0800

    cambio path de bkp

--- logbkp.sh
+++ logbkp.sh
@@ -7,7 +7,7 @@ bkp () {
 #DIR=$HOME
 
 DATE=$(date +%d-%m-%Y-%H%M%S)
-BACKUP_DIR=/home/alejandro/alejandro-areco/trabajos/final
+BACKUP_DIR=/home/alejandro/final-programacion
 SOURCE=/usr/local/nagios/var/nagios.log
 
 tar -cvzpf $BACKUP_DIR/nagios-log-BKP-$DATE.tar.gz $SOURCE
